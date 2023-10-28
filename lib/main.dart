eedb646a5b8f7d1aabb262e4899fa34d079b8e91
dina mr <dinamaulidaturrahma210@gmail.com>
2023-10-28 16:42:25 +0700

    progress3

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,61 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_uts/page/HomePage.dart';
-import 'package:flutter_application_uts/page/PendingPage.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(scaffoldBackgroundColor: Colors.white),
-      routes: {
-        "/": (context) => HomePage(
-          
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(""),
+          backgroundColor: Colors.white,
+          actions: <Widget>[
+            IconButton(
+              icon: Image.asset(
+                'assets/linkaja.png', // Ganti dengan path gambar Anda
+                width: 30, // Lebar gambar
+                height: 30,
+                alignment: Alignment.topLeft, // Tinggi gambar
+              ),
+              onPressed: () {
+                // Tambahkan fungsi yang akan dijalankan saat gambar/ikon ditekan
+              },
+            ),
+          ],
         ),
-        "PendingPage": (context) => PendingPage(),
-      },
+        body: ListView(
+          children: <Widget>[
+            Container(
+              color: Colors.red,
+              alignment: Alignment.topLeft,
+              child: Text(
+                "Halo, Dina Rahma",
+                style: TextStyle(fontSize: 30, color: Colors.white),
+              ),
+            ),
+            Container(
+              color: Colors.white,
+              alignment: Alignment.topLeft,
+              child: Text(
+                "",
+                style: TextStyle(fontSize: 30, color: Colors.black), // Ganti warna teks sesuai preferensi Anda
+              ),
+            ),
+            Container(
+              color: Colors.white,
+              alignment: Alignment.topLeft,
+              child: Text(
+                "",
+                style: TextStyle(fontSize: 30, color: Colors.black), // Ganti warna teks sesuai preferensi Anda
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
-
-
-
